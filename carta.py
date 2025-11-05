05692fd55131e9a01f4446ac0ddb9827d1df6098
2btc15 <2btc15@iesramonllull.local>
2025-11-05 09:49:23 +0100

    creado main

--- carta.py
+++ carta.py
@@ -1,1 +1,7 @@
 # Clase carta
+class Carta:
+    def __init__(self, palo, valor): #constructor
+        self.palo = palo
+        self.valor = valor
+    def __repr__(self): #método para mostrar la carta
+        return f"{self.valor} de {self.palo}"
